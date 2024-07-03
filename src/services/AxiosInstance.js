4cc1b3b8de65aaa50a8b9a4ece2d3d94227ae3b0
JoshuaJohnson8848 <joshuatjohnson255@gmail.com>
2024-07-03 16:49:31 +0530

    API Change

--- src/services/AxiosInstance.js
+++ src/services/AxiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:7000/',
+    // baseURL: 'http://localhost:7000/',
+    baseURL: 'http://192.168.1.2:7000/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json'
