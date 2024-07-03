282f269db577a0b105be4e96fb2ff16d41086d67
JoshuaJohnson8848 <joshuatjohnson255@gmail.com>
2024-07-03 19:30:34 +0530

    Vercel restored

--- src/services/AxiosInstance.js
+++ src/services/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL: 'http://localhost:7000/',
-    baseURL: 'https://invact-node-joshuajohnson8848s-projects.vercel.app/api/',
+    baseURL: 'https://invact-node-msn1r474t-joshuajohnson8848s-projects.vercel.app/api/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json'
